57398589b746ef50fa007686bbd4ca9df4f41b24
Yann <85126846+Yann-ship-it@users.noreply.github.com>
2022-10-06 16:54:53 +0200

    Tableau

--- src/com/koko/Conditions.java
+++ src/com/koko/Conditions.java
@@ -8,7 +8,7 @@ public class Conditions {
     int money = 1000; // J'initialise ma variable money à 1000
         if(money >= 5000) { // Si la money est supérieur ou égale à 5000 donc
         System.out.print(" Tu peux "); //
-    }  else if(money == 2000) { // Sinon si ma variable money est égale à 2000
+    }  else if(money <= 2000) { // Sinon si ma variable money est égale à 2000
         System.out.print(" Tu ne peux vraiment pas "); //
     } else { // Sinon
         System.out.print(" C'est mort "); //
--- src/com/koko/Main.java
+++ src/com/koko/Main.java
@@ -4,9 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String pseudo = "Savenko";
-        String[] names = new String[] {"Savenko", "Yann", "Jean"};
-        System.out.println(pseudo);
-
     }
 }
