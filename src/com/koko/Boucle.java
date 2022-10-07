86e59c0214ceba64204255f235eca1808a1c3d8b
Yann <85126846+Yann-ship-it@users.noreply.github.com>
2022-10-07 15:28:23 +0200

    Boucle

---
+++ src/com/koko/Boucle.java
@@ -0,0 +1,12 @@
+package com.koko;
+
+public class Boucle {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 100; i++) {
+            System.out.println("Boucle java" + " " + i);
+        }
+    }
+
+}
