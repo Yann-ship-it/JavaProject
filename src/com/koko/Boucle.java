676bc6ead1f772bb0e71feb7a9698adad88e4bec
Yann <85126846+Yann-ship-it@users.noreply.github.com>
2022-10-07 15:35:12 +0200

    Boucle

--- src/com/koko/Boucle.java
+++ src/com/koko/Boucle.java
@@ -7,6 +7,21 @@ public class Boucle {
         for (int i = 0; i <= 100; i++) {
             System.out.println("Boucle java" + " " + i);
         }
+
+        String pseudoss = "Zebi,La,If,Nice";
+        String[] pseudos = pseudoss.split(",");
+        int[] notes = {7, 9, 10, 5, 9};
+        int calcul = 0;
+
+        for(int note : notes){
+            calcul = calcul + note;
+        }
+        System.out.println(calcul / notes.length);
+        System.out.println(notes.length);
+
+        for(String str : pseudos) {
+            System.out.println(str);
+        }
     }
 
 }
--- src/com/koko/Main.java
+++ src/com/koko/Main.java
@@ -3,19 +3,6 @@ package com.koko;
 public class Main {
 
     public static void main(String[] args) {
-        String pseudoss = "Zebi,La,If,Nice";
-        String[] pseudos = pseudoss.split(",");
-        int[] notes = {7, 9, 10, 5, 9};
-        int calcul = 0;
 
-        for(int note : notes){
-            calcul = calcul + note;
-        }
-        System.out.println(calcul / notes.length);
-        System.out.println(notes.length);
-
-        for(String str : pseudos) {
-            System.out.println(str);
-        }
     }
 }
